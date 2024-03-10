190c45a0ff44a51a1c33d1fbf1cff4ca50e04421
KakashiPlusPlus <alvaro.mortellado@gmail.com>
2024-03-10 18:42:00 -0300

    I found the original pwm library, I add the info on README.md and delete and replace the old files of the library. I only keep the example code and the link of the video. I also start coding to use the A6275 with this library.

--- PWM_signal_Arduino/PWM_signal_Arduino.ino
+++ PWM_signal_Arduino/PWM_signal_Arduino.ino
@@ -1,9 +1,45 @@
-void setup() {
-  // put your setup code here, to run once:
 
+
+/*
+PWM.h
+
+ Mimics the fade example but with an extra parameter for frequency. It should dim but with a flicker 
+ because the frequency has been set low enough for the human eye to detect. This flicker is easiest to see when 
+ the LED is moving with respect to the eye and when it is between about 20% - 60% brighness. The library 
+ allows for a frequency range from 1Hz - 2MHz on 16 bit timers and 31Hz - 2 MHz on 8 bit timers. When 
+ SetPinFrequency()/SetPinFrequencySafe() is called, a bool is returned which can be tested to verify the 
+ frequency was actually changed.
+ 
+ This example runs on mega and uno.
+ */
+
+
+int32_t frequency = 1;  // Set the frequency(HZ), can work with 10Hz to 300kHz aprox
+#include <PWM.h>
+#define outputEnable 4  //Pin the enable A6275's led's outputs
+#define clk 9 //Pin who makes the clk signal for A627
+#define latchEnable 8 //Pin enable the A6275's latches (when is high) 
+#define serialDataOut 2 //Point to A6275's serialDataIn pin
+
+
+void setup() {
+  InitTimersSafe(); 
+  Serial.begin(9600);
+  bool success = SetPinFrequencySafe(9, frequency);
+
+  int sensorValue = analogRead(A4);
+
+  if(success) {
+    //When arduino's led "L" is high, means success
+    pinMode(13, OUTPUT);
+    digitalWrite(13, HIGH);
+    Serial.println("SetPinFrequencySafe: Success");
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  sensorValue = analogRead(A4); //Change A4 value, change the duty cycle
+  
+  pwmWrite(9, sensorValue / 4);  //pin 9 is the output clk pin
+  delay(30);
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+Example video found in yt
+https://www.youtube.com/watch?v=v5fWs7u2LDU
+
+Library original from Sam Knight (PWM.h)
+https://github.com/RCS101/PWM
