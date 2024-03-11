62b6a4ec60081c891ad032b19da238aa6c0c0dc1
KakashiPlusPlus <alvaro.mortellado@gmail.com>
2024-03-10 21:01:36 -0300

    I have been doing some test with the timing of arduino and A6275 in different frequencys and duty cycle. Should improve the reliability, but works ok I low frequencys. I take picture of the problem. High duty cycle help with the problem but is not ideal I think.

--- PWM example in arduino/PWM_en_arduino/PWM_en_arduino.ino
+++ PWM example in arduino/PWM_en_arduino/PWM_en_arduino.ino
@@ -1,14 +1,20 @@
 #include <PWM.h>
 
 
-int32_t frequency = 1000; // Establezca la Frecuencia en Hertz (HZ), se pueden operar frecuencias de 10Hz a 300kHz aprox
+int32_t frequency = 10; // Establezca la Frecuencia en Hertz (HZ), se pueden operar frecuencias de 10Hz a 300kHz aprox
+bool flag = true;
+unsigned long time; 
+
+
 void setup()
 {
   InitTimersSafe(); 
 
   //Establece la frecuencia para el pin especificado
   bool success = SetPinFrequencySafe(9, frequency);
-  
+  pinMode(3, OUTPUT);
+
+
   //SI LA FRECUENCIA DEL PIN SE CONFIGURO CORRECTAMENTE, ENCIENDA EL PIN 13.
   if(success) {
     pinMode(13, OUTPUT);
@@ -22,4 +28,19 @@ void loop()
                                     //(Lado izquierdo(pin 1) a GND y lado derecho (Pin 3) a +5V
   pwmWrite(9, sensorValue / 4);     //la terminal de enmedio(2) conectarla a una entrada analogica (A2)
   delay(30);                        //La salida PWM será el pin digital (9) con respecto a tierra.
+
+  if(flag == true && analogRead(A3) >= 220){
+    digitalWrite(3, HIGH);
+    flag = false;
+  }else{
+    flag = true;
+  }
+
+  if(analogRead(A3) <=100){
+    digitalWrite(3, LOW);
+    flag = false;
+  }
+
+
+
 }
